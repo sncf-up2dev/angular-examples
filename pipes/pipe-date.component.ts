1da6896ccd449ff7940642a82c0f374a0322a4c7
MVeyrandSncf <ext.mathieu.veyrand@sncf.fr>
2023-12-07 15:26:01 +0100

    Ajout exemples pipes

---
+++ pipes/pipe-date.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-example',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div> Date avec un nombre : {{ dateNumber | date }}</div>
+    <div> Date avec un objet Date : {{ dateObj | date }}</div>
+    
+    <button (click)="addTime()" >Ajouter</button>
+  `,
+  styles: ['']
+})
+export class PipeDateComponent {
+
+  readonly DAY = 24*60*60*1000
+
+  dateObj: Date = new Date()
+  dateNumber: number = this.dateObj.getTime();
+
+  addTime() {
+    this.dateNumber += this.DAY
+    this.dateObj.setTime(this.dateNumber)
+
+    console.log(this.dateNumber)
+    console.log(this.dateObj)
+  }
+
+}
---
+++ pipes/pipes-performance.component.ts
@@ -0,0 +1,41 @@
+import { Component, Pipe, PipeTransform } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Pipe({
+  name: 'toUpperCase',
+  standalone: true
+})
+export class ToUpperCasePipe implements PipeTransform {
+
+  transform(value: string): string {
+    console.log("appel pipe")
+    return value.toUpperCase();
+  }
+
+}
+
+@Component({
+  selector: 'app-example',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ToUpperCasePipe],
+  template: `
+    <div class="border" (mouseover)="0">
+      <input [(ngModel)]="texte" />
+
+      <div>{{ toUpperCase(texte) }}</div>
+      <div>{{ texte | toUpperCase }}</div>
+    </div>
+  `,
+  styleUrls: []
+})
+export class PipesPerformanceComponent {
+
+  toUpperCase(input: string): string {
+    console.log("appel function")
+    return input.toUpperCase();
+  }
+
+  texte = 'Exemple de texte'
+
+}
